ebb4293fb41e7d9e1ffcf3a543b335f9c5b7f7e4
marycross3 <marycradick@gmail.com>
2023-03-05 17:28:36 -0600

    done

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 // Write your JavaScript code here!
 
+// const { pickPlanet, addDestinationInfo } = require("./scriptHelper");
+
+// const { myFetch } = require("./scriptHelper");
+
+// const { formSubmission } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+    let form = document.querySelector("form");
+    let list = document.getElementById("faultyItems");
+    list.style.visibility = "hidden";
+
+
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        let planetPicked = pickPlanet(listedPlanets);
+        addDestinationInfo(document,planetPicked.name, planetPicked.diameter,planetPicked.star,planetPicked.distance, planetPicked.moons, planetPicked.image);
+    });
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let pilot = document.querySelector("input[name=pilotName]").value;
+    let copilot = document.querySelector("input[name=copilotName]").value;
+    let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+    let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+    formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+    
+});
+        // Below this comment call the appropriate helper functions to pick a planet fom 
+        //the list of planets and add that information to your destination.
+    
+
 });
